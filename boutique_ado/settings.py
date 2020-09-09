04d2aeafa4a4cf6551e5529aa979cee5665f0b5d
singeroi <bonjour_robert@outlook.com>
2020-09-09 22:51:36 +0200

    adds mobile header and main navbar

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -147,3 +147,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATICFILES_DIRS is a tuple hence (),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#to allow django to see the media url need to go to urls.py
