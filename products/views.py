529a29b73a4b75ce9331e2d6a25b30b4a792d872
singeroi <bonjour_robert@outlook.com>
2020-09-13 17:11:14 +0200

    adds category tags, adds sort select functionality, adds sort by name

--- products/views.py
+++ products/views.py
@@ -27,6 +27,8 @@ def all_products(request):
                 sortkey = 'lower_name'
                 #to annotate products
                 products  = products.annotate(lower_name=Lower('name'))
+            if sortkey =='category':
+                sortkey == 'category__name' #__syntax allows drilling into related model
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
