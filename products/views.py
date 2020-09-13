ddd096053d79219ee687b9c856cca533f93d6598
singeroi <bonjour_robert@outlook.com>
2020-09-13 13:33:44 +0200

    adds sort functionality

--- products/views.py
+++ products/views.py
@@ -12,16 +12,35 @@ def all_products(request):
     # to set initial stateof query to none
     query = None
     categories = None
+    sort = None
+    direction = None
 
     #to take search requests
     if request.GET:
+        #for sorting
+        if 'sort' in request.GET:
+            sortkey= request.GET['sort']
+            sort = sortkey
+            #sets sort from none to sortkey
+            #for case insensitive sorting
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                #to annotate products
+                products  = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         #for category search
         if 'category' in request.GET:
             categories = request.GET['category'].split(',') #split query at commas (where appropriate) and use to search all present products
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
-    if 'q' in request.GET:
+        if 'q' in request.GET:
             query = request.GET['q']
             #to handle blank searches
             if not query:
@@ -33,10 +52,13 @@ def all_products(request):
             # adds queries var to the filter
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current-sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
